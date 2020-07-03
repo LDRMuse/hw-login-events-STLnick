f829be4d94be63e9dce0d33a6d8a01af442f3a80
Nick <nickrray@gmail.com>
2020-07-03 11:20:37 -0500

    :sparkles: Add input field event listeners

--- client/scripts.js
+++ client/scripts.js
@@ -1,2 +1,20 @@
 // TODO: 'Grab' your form and add an event listener.
 // Listen for 'blur' and 'focus.' What do these do/mean?
+
+// 'Focus' means to click inside, or focus on, the element
+// 'Blur' means to or leave that element
+const username = document.querySelector('#username')
+const password = document.querySelector('#password')
+
+username.addEventListener('focus', () => {
+  console.log('Focused on the username input!')
+})
+username.addEventListener('blur', () => {
+  console.log('The username field was left...')
+})
+password.addEventListener('focus', () => {
+  console.log('Focused on the password input!')
+})
+password.addEventListener('blur', () => {
+  console.log('The password field was left...')
+})
