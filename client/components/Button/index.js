e12b9eaa1ca72c1130f652efe8390afd4d9a0fc1
Nick Ray <nickrray@gmail.com>
2020-07-14 16:54:40 -0500

    :recycle: Change reduce() to map() + join() to generate button html

--- client/components/Button/index.js
+++ client/components/Button/index.js
@@ -1,7 +1,10 @@
 function render (buttons) {
-  return buttons.reduce((acc, btn) => {
-    return acc + `<button id="${btn.id}" type="${btn.type.toLowerCase()}">${btn.type}</button>`
-  }, '')
+  // return buttons.reduce((acc, btn) => {
+  //   return acc + `<button id="${btn.id}" type="${btn.type.toLowerCase()}">${btn.type}</button>`
+  // }, '')
+  return buttons.map((btn) => {
+    return `<button id="${btn.id}" type="${btn.type.toLowerCase()}">${btn.type}</button>`
+  }).join('')
 }
 
 export default (props) => render(props)
