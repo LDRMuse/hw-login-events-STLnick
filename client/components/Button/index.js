0e3129fc4d299ed7b14116cdf3644cba1decf448
Nick Ray <nickrray@gmail.com>
2020-07-14 16:03:38 -0500

    :bulb: Remove comments used to structure dynamic html string

--- client/components/Button/index.js
+++ client/components/Button/index.js
@@ -1,11 +1,7 @@
-function render(buttons) {
+function render (buttons) {
   return buttons.reduce((acc, btn) => {
     return acc + `<button id="${btn.id}" type="${btn.type.toLowerCase()}">${btn.type}</button>`
   }, '')
 }
 
 export default (props) => render(props)
-
-// <button id="resetBtn" type="reset">Reset</button>
-
-// <button id="submitBtn" type="submit">Login</button>
